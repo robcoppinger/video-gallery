3eacf12db2752d53854dbfef749252f96b9f977c
Rob Coppinger <rob@Robs-MacBook-Pro.local>
2020-03-21 23:00:01 +0000

    create sliding filtering side panel

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -3,14 +3,80 @@ import VideoList from "../data/VideoList.json";
 import { VideoCard } from "../Components/VideoCard.js";
 
 export const Gallery = () => {
-  console.log(VideoList);
+  const [filteredNames, setFilteredNames] = React.useState({});
+  const [showFilter, setShowFilter] = React.useState(false);
+
+  React.useEffect(() => {
+    const names = VideoList.map(video => {
+      return video.featuring.concat(video.starring);
+    })
+      .flat() //flatten to one array
+      .filter((value, index, self) => self.indexOf(value) === index) //unique
+      .sort(); //sorted
+
+    const filter = {};
+    for (var name of names) {
+      filter[name] = false;
+    }
+
+    setFilteredNames(filter);
+  }, []);
+
+  const filterVideos = () => {
+    const filteredArray = Object.keys(filteredNames).filter(
+      name => filteredNames[name] === true
+    );
+    if (filteredArray.length === 0) return VideoList;
+    return VideoList.filter(video => {
+      const names = video.starring.concat(video.featuring);
+      const intersection = names.filter(name => filteredArray.includes(name));
+      return intersection.length > 0;
+    });
+  };
+
+  const handleChange = e => {
+    setFilteredNames({ ...filteredNames, [e.target.name]: e.target.checked });
+  };
+
   return (
-    <div className="contentContainer gallery">
-      <div className="videoContentContainer">
-        {VideoList.map(video => (
-          <VideoCard key={video.id} video={video} />
-        ))}
-        <div style={{ flex: "1 0 21%", margin: "2rem" }} />
+    <div className="gallery">
+      <img
+        alt="filter"
+        className="filterImg"
+        src={require("../images/filter.png")}
+        onClick={() => setShowFilter(true)}
+      />
+      <div className="contentContainer">
+        <div className="videoContentContainer">
+          {filterVideos().map(video => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </div>
+      </div>
+
+      <div className="sidePanel" data-visible={showFilter}>
+        <div className="sidepanelHeader">
+          <h3>Filter by people</h3>
+          <img
+            src={require("../images/close.png")}
+            alt="close"
+            onClick={() => setShowFilter(false)}
+          />
+        </div>
+
+        <form>
+          {Object.keys(filteredNames).map(name => (
+            <label key={name}>
+              <input
+                type="checkbox"
+                name={name}
+                onChange={handleChange}
+                checked={filteredNames[name]}
+              />
+              &nbsp; {name} <br />
+            </label>
+          ))}
+        </form>
       </div>
     </div>
   );
