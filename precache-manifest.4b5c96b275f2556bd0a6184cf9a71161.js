666e631c7ec9bb4a552fc70bb5854ef1b43eadb6
Rob Coppinger <rob@Robs-MacBook-Pro.local>
2020-03-21 10:57:45 +0000

    Updates

---
+++ precache-manifest.4b5c96b275f2556bd0a6184cf9a71161.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842a6259074c932d88619cf63951c4d7",
+    "url": "/video-gallery/index.html"
+  },
+  {
+    "revision": "bce56fd3a1b5f22461dc",
+    "url": "/video-gallery/static/css/main.b0de6681.chunk.css"
+  },
+  {
+    "revision": "f9296ce81dffffe03afe",
+    "url": "/video-gallery/static/js/2.9828a557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-gallery/static/js/2.9828a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce56fd3a1b5f22461dc",
+    "url": "/video-gallery/static/js/main.41379a08.chunk.js"
+  },
+  {
+    "revision": "46ed509b2fc04cb10eed",
+    "url": "/video-gallery/static/js/runtime-main.284a0883.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-gallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
