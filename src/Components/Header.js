af677b7706fabc9ef1b2e96c95db84e47020b8e8
Rob Coppinger <rob@Robs-MacBook-Pro.local>
2020-03-22 23:26:51 +0000

    add in Logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export const Header = () => (
   <div className="header">
-    <div className="contentContainer">
+    <div className="contentContainer headerContainer">
+      <img src={require("../images/logo.png")} />
       <h1>Anzere Ski Trip 2020</h1>
     </div>
   </div>
